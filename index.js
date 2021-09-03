b94b040db28b8d9cdb0ca524c19ba1d6e93fe258
Shashi <shashikant.p@gmail.com>
2021-09-03 22:48:44 +0530

    Db Integration

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const expresshb = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
 
 mongoose.connect('mongodb://localhost:27017/testdb');
 
 const personCollection = new mongoose.Schema({
-    fname: String, // String is shorthand for {type: String}
+    email: String, 
+    password: String,
+    fname: String,
     lname: String
 });
 
@@ -15,6 +19,8 @@ const app = express();
 
 app.engine('handlebars', expresshb());
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const names = [{
     "fname": "Test 1",
@@ -42,6 +48,62 @@ app.get("/", function (req, res) {
         admin: true,
         layout: "main"
     });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login", {});
+});
+
+app.get("/reg", function (req, res) {
+    res.render("registration", {});
+});
+
+app.post("/loginsubmit", async function (req, res) {
+
+
+    console.log(req.body.email);
+    console.log(req.body.pass);   
+    
+    const person = await PersonModel.findOne({
+        email: req.body.email
+    }).exec();
+
+    if (person) {
+        console.log("Match found");
+        console.log(person.password);
+        if (person.password == req.body.pass) {
+            console.log("Pass match");
+            res.render("test", {});
+        } else {
+            console.log("Pass mismatch");
+            res.render("login", {
+                message: "Invalid credentails"
+            });
+        }
+    } else {
+        res.render("login", {
+            message: "Invalid credentails"
+        });        
+    }
+});
+
+app.post("/regsubmit", async function (req, res) {
+    console.log(req.body);
+    console.log(req.body.email);
+    console.log(req.body.fname);
+    console.log(req.body.lname);     
+    console.log(req.body.pass);      
+
+    const person = new PersonModel({
+        email: req.body.email, 
+        password: req.body.pass,
+        fname: req.body.fname,
+        lname: req.body.lname        
+    });
+
+    await person.save();
+
+    res.render("login", {});
 });
 
 app.listen(3003);
